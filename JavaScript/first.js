23e87c78afde25886ee720481f1601b815406b01
Naresh Chaurasia <johndoe@example.com>
2024-04-27 19:17:31 +0530

    Macbook Pro::Sat Apr 27 19:17:31 IST 2024

--- JavaScript/first.js
+++ JavaScript/first.js
@@ -1,9 +1,2 @@
-const person = {
-    name: "Alice",
-    greet: function(name) {
-        this.name = name;
-        console.log("Hello, my name is " + person.name);
-    }
-};
-
-person.greet('NC'); // Output: Hello, my name is Alice
+let count=1;
+console.log(count);
