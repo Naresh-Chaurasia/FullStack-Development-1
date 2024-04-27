d6eb22a5ac77f989b481152de09ddeced943cd37
Naresh Chaurasia <johndoe@example.com>
2024-04-27 15:30:48 +0530

    Macbook Pro::Sat Apr 27 15:30:48 IST 2024

---
+++ JavaScript/first.js
@@ -0,0 +1,9 @@
+let arr1 = [ 1, 2, 3, 4, 5 ];
+let arr2 = arr1;
+console.log(arr1); // [ 1, 2, 3, 4, 5 ]
+console.log(arr2); // [ 1, 2, 3, 4, 5 ]
+ 
+// append an element to the array
+arr1.push(6);
+console.log(arr1); //[ 1, 2, 3, 4, 5, 6 ]
+console.log(arr2); //[1, 2, 3, 4, 5, 6 ]
