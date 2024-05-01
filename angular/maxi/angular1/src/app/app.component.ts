6a8d094a180778d74ee80df85e825705e60bfd2a
Naresh Chaurasia <johndoe@example.com>
2024-05-01 08:08:08 +0530

    Macbook Pro::Wed May  1 08:08:08 IST 2024

---
+++ angular/maxi/angular1/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  template: '<app-servers></app-servers>',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular1';
+}
